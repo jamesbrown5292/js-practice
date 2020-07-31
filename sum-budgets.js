e263db423855c60fac29d2e8c1189cef67987380
James Brown <jamesbrown5292@gmail.com>
2020-07-31 14:47:21 +0000

    completed a level medium edabit challenge in 41 mint

--- sum-budgets.js
+++
@@ -1,9 +0,0 @@
-let getBudgets = function (objectsArray){
-  let total = 0;
-  for (let i=0; i<objectsArray.lengthl i++){
-    total += objectsArray[i].budget;
-  }
-  return total;
-}
-
-//edabit level medium time taken 7 minutes
